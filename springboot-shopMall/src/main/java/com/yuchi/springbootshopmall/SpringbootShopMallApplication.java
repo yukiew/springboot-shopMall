6e46bf1029eb77a57772f94fee5cb1945912cf32
yuser <sd11gg26@gmail.com>
2023-09-13 01:07:39 +0800

    init springboot project

---
+++ springboot-shopMall/src/main/java/com/yuchi/springbootshopmall/SpringbootShopMallApplication.java
@@ -0,0 +1,13 @@
+package com.yuchi.springbootshopmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShopMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShopMallApplication.class, args);
+    }
+
+}
---
+++ springboot-shopMall/src/test/java/com/yuchi/springbootshopmall/SpringbootShopMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuchi.springbootshopmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShopMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
